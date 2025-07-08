10ef46b2ae22b4e9c2001b412ef381e4ee1ff933
sahil20bestpeers <schourasiya@bestpeers.com>
2025-07-08 19:25:41 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,22 @@
+# Create a module with a function square() and import it in another file.
+
+# Write a program to open a file and read its contents using with context manager.
+
+# Write a program to write a string into a file.
+
+# Append a line to an existing file.
+
+# Handle division by zero using exception handling.
+
+# Handle invalid user input (like string input instead of number).
+
+# Write a program that reads from a file and counts the number of words.
+
+
+
+
+def square(x):
+    return x*x
+x = int(input("enter a number x "))
+result = square(x)
+print(result)
